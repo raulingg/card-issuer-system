98f219f57ec3c125f1ce25451329f3a2793cdad1
Raul Mendez <relaxedchild@gmail.com>
2026-03-12 11:08:45 -0500

    refactor: remove unused rpc-exception filter export from index

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,5 +1,4 @@
 export * from './filters/http-exception.filter';
-export * from './filters/rpc-exception.filter';
 export * from './interceptors/logging.interceptor';
 export * from './pipes/zod-validation.pipe';
 export * from './dto/card.dto';
